20210039ec72b08fdeac670c6e411d13824e41d8
Maximilian Ruta <mr@deltachaos.de>
2025-03-28 01:29:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -86,8 +86,8 @@ class FastAPI:
 
         handler, params = self.match_route(path, request.method)
         if handler:
-            merged_params = {**query, **params}
-            merged_params = {k: v for k, v in merged_params.items()}
+            merged_params = {k: v[0] for k, v in query.items()}
+            merged_params = {**merged_params, **params}
             response = await handler(**merged_params)
             if not isinstance(response, CloudflareResponse):
                 response = JsonResponse(response)
